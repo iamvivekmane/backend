fe0673a9713cd599891d8bb02638fd1cececd74c
Vivek Mane <vivekmane172@gmail.com>
2025-07-02 21:24:59 +0530

    Clear clutter

--- Exercise 01/main.js
+++ Exercise 01/main.js
@@ -1,102 +1,103 @@
+const fs = require("fs/promise");
+const fsn = require("fs");
 const path = require("path");
-const fs = require("fs");
 console.log("hello");
 
-let oldPaths = [
-  "d:\\Code\\Backend\\Backend\\Exercise 01\\another.zip",
-  "d:\\Code\\Backend\\Backend\\Exercise 01\\cat.jpg",
-  "d:\\Code\\Backend\\Backend\\Exercise 01\\name.jpg",
-  "d:\\Code\\Backend\\Backend\\Exercise 01\\name.png",
-  "d:\\Code\\Backend\\Backend\\Exercise 01\\random.zip",
-  "d:\\Code\\Backend\\Backend\\Exercise 01\\this.pdf",
-];
+let basePath = "D:\\Code\\Backend\\Backend\\Exercise 01";
 
-let folderNames = [
-  "d:\\Code\\Backend\\Backend\\Exercise 01\\zip",
-  "d:\\Code\\Backend\\Backend\\Exercise 01\\jpg",
-  "d:\\Code\\Backend\\Backend\\Exercise 01\\png",
-  "d:\\Code\\Backend\\Backend\\Exercise 01\\pdf",
-];
+let files = await fs.readdir(basePath);
+console.log(files);
 
-for (let i = 0; i < oldPaths.length; i++) {
-  if (path.extname(oldPaths[i]) == ".zip") {
-    try {
-      if (!fs.existsSync(folderNames[0])) {
-        fs.mkdirSync(folderNames[0]);
-        console.log("Folder created succcessfully");
-      }
-    } catch (err) {
-      console.log(err);
-    }
+// let folderNames = [
+//   "d:\\Code\\Backend\\Backend\\Exercise 01\\zip",
+//   "d:\\Code\\Backend\\Backend\\Exercise 01\\jpg",
+//   "d:\\Code\\Backend\\Backend\\Exercise 01\\png",
+//   "d:\\Code\\Backend\\Backend\\Exercise 01\\pdf",
+// ];
 
-    let newPath = folderNames[0] + "\\" + path.basename(oldPaths[i]);
-    console.log(newPath);
+// for (const item of files) {
+//   let ext = path.basename(item);
+//   console.log(ext);
+// }
 
-    fs.rename(oldPaths[i], newPath, function (err) {
-      if (err) {
-        throw err;
-      }
-      console.log("Success!");
-    });
-  } else if (path.extname(oldPaths[i]) == ".jpg") {
-    try {
-      if (!fs.existsSync(folderNames[1])) {
-        fs.mkdirSync(folderNames[1]);
-        console.log("Folder created succcessfully");
-      }
-    } catch (err) {
-      console.log(err);
-    }
+// if (path.extname(item) == ".zip") {
+//   try {
+//     if (!fs.existsSync(folderNames[0])) {
+//       fs.mkdirSync(folderNames[0]);
+//       console.log("Folder created succcessfully");
+//     }
+//   } catch (err) {
+//     console.log(err);
+//   }
 
-    let newPath = folderNames[1] + "\\" + path.basename(oldPaths[i]);
-    console.log(newPath);
+//   let newPath = folderNames[0] + "\\" + path.basename(oldPaths[i]);
+//   console.log(newPath);
 
-    fs.rename(oldPaths[i], newPath, function (err) {
-      if (err) {
-        throw err;
-      }
-      console.log("Success!");
-    });
-  } else if (path.extname(oldPaths[i]) == ".png") {
-    try {
-      if (!fs.existsSync(folderNames[2])) {
-        fs.mkdirSync(folderNames[2]);
-        console.log("Folder created succcessfully");
-      }
-    } catch (err) {
-      console.log(err);
-    }
+//   fs.rename(oldPaths[i], newPath, function (err) {
+//     if (err) {
+//       throw err;
+//     }
+//     console.log("Success!");
+//   });
+// } else if (path.extname(oldPaths[i]) == ".jpg") {
+//   try {
+//     if (!fs.existsSync(folderNames[1])) {
+//       fs.mkdirSync(folderNames[1]);
+//       console.log("Folder created succcessfully");
+//     }
+//   } catch (err) {
+//     console.log(err);
+//   }
 
-    let newPath = folderNames[2] + "\\" + path.basename(oldPaths[i]);
-    console.log(newPath);
+//   let newPath = folderNames[1] + "\\" + path.basename(oldPaths[i]);
+//   console.log(newPath);
 
-    fs.rename(oldPaths[i], newPath, function (err) {
-      if (err) {
-        throw err;
-      }
-      console.log("Success!");
-    });
-  } else if (path.extname(oldPaths[i]) == ".pdf") {
-    try {
-      if (!fs.existsSync(folderNames[3])) {
-        fs.mkdirSync(folderNames[3]);
-        console.log("Folder created succcessfully");
-      }
-    } catch (err) {
-      console.log(err);
-    }
+//   fs.rename(oldPaths[i], newPath, function (err) {
+//     if (err) {
+//       throw err;
+//     }
+//     console.log("Success!");
+//   });
+// } else if (path.extname(oldPaths[i]) == ".png") {
+//   try {
+//     if (!fs.existsSync(folderNames[2])) {
+//       fs.mkdirSync(folderNames[2]);
+//       console.log("Folder created succcessfully");
+//     }
+//   } catch (err) {
+//     console.log(err);
+//   }
 
-    let newPath = folderNames[3] + "\\" + path.basename(oldPaths[i]);
-    console.log(newPath);
+//   let newPath = folderNames[2] + "\\" + path.basename(oldPaths[i]);
+//   console.log(newPath);
 
-    fs.rename(oldPaths[i], newPath, function (err) {
-      if (err) {
-        throw err;
-      }
-      console.log("Success!");
-    });
-  }
-}
+//   fs.rename(oldPaths[i], newPath, function (err) {
+//     if (err) {
+//       throw err;
+//     }
+//     console.log("Success!");
+//   });
+// } else if (path.extname(oldPaths[i]) == ".pdf") {
+//   try {
+//     if (!fs.existsSync(folderNames[3])) {
+//       fs.mkdirSync(folderNames[3]);
+//       console.log("Folder created succcessfully");
+//     }
+//   } catch (err) {
+//     console.log(err);
+//   }
+
+//   let newPath = folderNames[3] + "\\" + path.basename(oldPaths[i]);
+//   console.log(newPath);
+
+//   fs.rename(oldPaths[i], newPath, function (err) {
+//     if (err) {
+//       throw err;
+//     }
+//     console.log("Success!");
+//   });
+// }
+// }
 
 // try {
 //   if (!fs.existsSync(folderName1)) {
